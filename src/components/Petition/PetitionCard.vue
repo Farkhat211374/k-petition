6f9aca08b3ae72bf1ca2a85f31c7ec5c1ced5686
Farkhat211374 <sagatfarhat@gmail.com>
2024-09-16 12:39:00 +0500

    test commit

--- src/components/Petition/PetitionCard.vue
+++ src/components/Petition/PetitionCard.vue
@@ -1,6 +1,5 @@
 <script setup>
-import thumbs from "../../assets/like.svg"
-
+import thumbs from "../../assets/like.svg";
 </script>
 
 <template>
@@ -12,26 +11,37 @@ import thumbs from "../../assets/like.svg"
       <q-card-section>
         <div class="text-overline text-orange-9">08.07.2024</div>
         <div class="text-caption text-grey q-mt-xs">Наименование проблемы</div>
-        <div class="text-h5 q-mb-md text-blue-10 text-weight-bold">Организация зон отдыха и релаксации в офисах</div>
+        <div class="text-h5 q-mb-md text-blue-10 text-weight-bold">
+          Организация зон отдыха и релаксации в офисах
+        </div>
         <div class="text-caption text-grey">Подробное описание</div>
         <div class="text-body1">
-          Отсутствие специально оборудованных зон отдыха в офисах приводит к переутомлению и стрессу у сотрудников, что отрицательно влияет на их эффективность и мотивацию
+          Отсутствие специально оборудованных зон отдыха в офисах приводит к
+          переутомлению и стрессу у сотрудников, что отрицательно влияет на их
+          эффективность и мотивацию
         </div>
-        <div class="q-mt-xl q-mb-md" >
+        <div class="q-mt-xl q-mb-md">
           <div class="text-caption text-grey">Направление деятельности</div>
-          <div class="text-subtitle1 text-blue-10 text-weight-bold">Бытовые условия</div>
+          <div class="text-subtitle1 text-blue-10 text-weight-bold">
+            Бытовые условия
+          </div>
           <div class="text-caption text-grey">Классификация проблемы</div>
-          <div class="text-subtitle1 text-blue-10 text-weight-bold">Материальное обеспечение</div>
+          <div class="text-subtitle1 text-blue-10 text-weight-bold">
+            Материальное обеспечение
+          </div>
           <div class="text-caption text-grey">Автор проблемы</div>
-          <div class="text-subtitle1 text-blue-10 text-weight-bold">Сотрудник службы ИБ - Сағат Ф. И.</div>
+          <div class="text-subtitle1 text-blue-10 text-weight-bold">
+            Сотрудник службы ИБ - Сағат Ф. И.
+          </div>
         </div>
 
-        <div class="text-subtitle1 text-positive text-weight-bold">Завершена 01.08.2024</div>
+        <div class="text-subtitle1 text-positive text-weight-bold">
+          Завершена 01.08.2024
+        </div>
 
-<!--        <div>-->
-<!--          <img :src="thumbs" height="30px" alt="thumbs">-->
-<!--        </div>-->
-
+        <!--        <div>-->
+        <!--          <img :src="thumbs" height="30px" alt="thumbs">-->
+        <!--        </div>-->
       </q-card-section>
       <q-card-actions>
         <q-btn flat color="primary" label="Подробнее" />
