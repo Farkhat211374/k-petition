a4691bef5b2f81ec1dc838341e5cc4fe89fb4ad3
Farkhat211374 <sagatfarhat@gmail.com>
2024-09-18 17:11:44 +0500

    new design start

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+    <q-header>
       <q-toolbar>
         <q-btn
           flat
@@ -11,7 +11,7 @@
           @click="toggleLeftDrawer"
         />
 
-        <q-toolbar-title> Quasar App </q-toolbar-title>
+        <q-toolbar-title> АРМ Проблема </q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,8 +1,10 @@
 <script setup>
+import ProblemCards from "src/components/ProblemCards.vue";
 import { ref } from "vue";
-import CreatePetitionDialog from "../components/Petition/CreatePetitionDialog.vue";
-import PetitionCardList from "components/Petition/PetitionCardList.vue";
+
 const confirm = ref(false);
+const options = ref("all");
+const search = ref("");
 
 const onClickCreate = () => {
   confirm.value = true;
@@ -15,21 +17,59 @@ const close = () => {
 
 <template>
   <div class="wrapper">
-    <q-btn
-      class="q-ml-md"
-      color="primary"
-      icon="my_location"
-      label="Добавить предложение"
-      @click="onClickCreate"
-    />
-    <CreatePetitionDialog :confirm="confirm" @close="close" />
-
-    <PetitionCardList />
+    <div class="q-gutter-y-md">
+      <div class="q-gutter-x-lg row items-start">
+        <q-input
+          class="col-6"
+          bg-color="white"
+          color="info"
+          v-model="search"
+          debounce="500"
+          outlined
+          dense
+          clearable
+          placeholder="Поиск"
+        >
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+        <q-btn
+          padding="sm"
+          color="primary"
+          icon-right="add"
+          label="Создать"
+          class="text-weight-bold"
+          no-caps
+          @click="onClickCreate"
+        />
+        <div class="q-gutter-x-md">
+          <q-btn padding="sm" color="white" text-color="grey" icon="map" />
+          <q-btn padding="sm" color="white" text-color="grey" icon="today" />
+          <q-btn padding="sm" color="info" icon="format_size" />
+        </div>
+      </div>
+      <div class="row items-center text-weight-bold" style="color: #a0a3b7">
+        <q-option-group
+          v-model="options"
+          inline
+          :options="[
+            { label: 'Все проблемы', value: 'all' },
+            { label: 'Активные проблемы', value: 'active' },
+            { label: 'Завершенные проблемы', value: 'closed' },
+          ]"
+        />
+      </div>
+      <div class=""><ProblemCards /></div>
+    </div>
   </div>
 </template>
 
 <style scoped>
 .wrapper {
   padding: 20px;
+  background-color: #f1f0f5;
+  min-height: 95vh;
+  box-sizing: border-box;
 }
 </style>
